486101e82824f8a70cdfdd2d398e24b192716700
Brent Groves <brent.groves@gmail.com>
2025-12-24 07:57:40 -0500

    updated source code

--- q_z/shell_scripts/endday.sh
+++ q_z/shell_scripts/endday.sh
@@ -2,11 +2,20 @@
 pushd .
 cd ~/bin/shell_scripts
 
+echo "pulling repsys"
+./startday_repsys.sh
+
 echo "pushing repsys"
 ./endday_repsys.sh
 
+echo "pulling liokr"
+./startday_liokr.sh
+
 echo "pushing liokr"
 ./endday_liokr.sh
+
+echo "pulling biokr"
+./startday_biokr.sh
 
 echo "pushing biokr"
 ./endday_biokr.sh
