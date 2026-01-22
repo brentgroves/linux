42ac8f6fe42cb67a74bbbd83e11611f2c4107339
brentgroves <brent.groves@gmail.com>
2026-01-22 18:16:58 -0500

    updated source code

--- q_z/shell_scripts/freshstart_iokr.sh
+++ q_z/shell_scripts/freshstart_iokr.sh
@@ -18,6 +18,10 @@ git switch main
 cd ~/src/iokr/teams/team_20/goals/goal_10/key_results/kr_10/inititives/init_30/okrs
 git switch main
 
+## git switch main if detached head
+cd ~/src/iokr/teams/team_50/goals/goal_10/key_results/kr_10/inititives/init_10/dimcol
+git switch main
+
 # cd ~/src/iokr
 # git add -A
 # git commit -m "Freshstart commit"
